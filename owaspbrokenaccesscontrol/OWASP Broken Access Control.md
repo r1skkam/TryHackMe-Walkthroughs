e9fbcac44d0e3be699b58e06e6a13e9ab8a0d9f3
Kam Khan Sang <riskkam01@gmail.com>
2023-07-01 19:36:20 +0630

    vault backup: 2023-07-01 19:36:20

--- owaspbrokenaccesscontrol/OWASP Broken Access Control.md
+++ owaspbrokenaccesscontrol/OWASP Broken Access Control.md
@@ -6,3 +6,25 @@ Exploit Broken Access Control: Number 1 of the Top 10 web security risks.
 
 ![[Pasted image 20230701184738.png]]
 
+```
+ping 10.10.208.69
+```
+
+![[Pasted image 20230701185304.png]]
+
+![[Pasted image 20230701192241.png]]
+
+![[Pasted image 20230701192726.png]]
+
+![[Pasted image 20230701192910.png]]
+
+![[Pasted image 20230701192945.png]]
+
+![[Pasted image 20230701193047.png]]
+
+![[Pasted image 20230701193120.png]]
+
+![[Pasted image 20230701193216.png]]
+
+![[Pasted image 20230701193557.png]]
+
---
+++ owaspbrokenaccesscontrol/dashboard.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dashboard</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <div class="container">
+        <div class="content">
+            <a id="link" class="logout" href="logout.php">Logout</a>
+            <h1>Welcome, riskkam is</h1>
+            <h2>Announcements</h2>
+            <hr>
+            <div>
+                <div>
+                    <h3>Status Update Test</h3>
+                    <h4>by: admin</h4>
+                    <p>Application building in progress</p>
+                </div>
+                <hr>
+                <div>
+                    <h3>Report the bugs</h3>
+                    <h4>by: admin</h4>
+                    <p>Pls email me at admin@admin.com for any bugs that you will encounter. Thanks</p>
+                </div>
+                <hr>
+
+                <table style="width : 100%">
+                    <tr>
+                        <th>Online users</th>
+                    </tr>
+                    <tr>
+                        <td>admin@admin.com</td>
+                        <tr><tr>
+                        <td>r1skkam@local.io</td>
+                        <tr>                </table>
+            </div>
+        </div>
+    </div>    
+</body>
+</html>

3c6156c6e9b350988ca358437ba0f5a0d38558e2
Kam Khan Sang <58542375+r1skkam@users.noreply.github.com>
2024-03-06 09:50:18 +0630

    Create hacker.php

---
+++ chillhack/hacker.php
@@ -0,0 +1,25 @@
+<html>
+<head>
+<body>
+<style>
+body {
+  background-image: url('images/002d7e638fb463fb7a266f5ffc7ac47d.gif');
+}
+h2
+{
+        color:red;
+        font-weight: bold;
+}
+h1
+{
+        color: yellow;
+        font-weight: bold;
+}
+</style>
+<center>
+        <img src = "images/hacker-with-laptop_23-2147985341.jpg"><br>
+        <h1 style="background-color:red;">You have reached this far. </h2>
+        <h1 style="background-color:black;">Look in the dark! You will find your answer</h1> 
+</center>    
+</head>
+</html>
